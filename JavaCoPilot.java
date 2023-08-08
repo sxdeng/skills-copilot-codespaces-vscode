2f55ed985fa47d06dd752e8ff15f8c805f5d6f2b
sxdeng <dengxiao.9@gmail.com>
2023-08-08 03:03:40 +0000

    print class string

--- JavaCoPilot.java
+++ JavaCoPilot.java
@@ -3,6 +3,7 @@ public class JavaCoPilot {
 
     public static void main(String[] args) {
         System.out.println(new JavaCoPilot().isTen(10));
+        System.out.println(new JavaCoPilot());
     }
 
     private boolean isTen(int a) {
